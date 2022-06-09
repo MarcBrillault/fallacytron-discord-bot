893c478930fa5849a62d36b34ddbe307ffb3b244
Marc Brillault <marc@brillault.fr>
2022-06-09 11:00:46 +0200

    Followed Gab's code review remarks

--- bot.py
+++ bot.py
@@ -29,15 +29,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if f'<@{client.user.id}>' in message.content:
-        apiResponse = requests.get(API_ROOT + API_PATH).json()
+    if client.user.mentioned_in(message):
+        req = requests.get(API_ROOT + API_PATH)
+        if req.status_code != 200:
+            return
 
-        imageResponse = requests.get(API_ROOT+apiResponse['image'])
-        open(TMP_IMAGE, 'wb').write(imageResponse.content)
+        api_response = req.json()
+        image_response = requests.get(API_ROOT + api_response['image'])
+        open(TMP_IMAGE, 'wb').write(image_response.content)
 
         file = discord.File(TMP_IMAGE)
-
         await message.channel.send(file=file)
+
         os.remove(TMP_IMAGE)
 
 client.run(TOKEN)
